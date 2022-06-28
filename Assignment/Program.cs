5475e9552f1770a5e19589da1c9ae09bea8e8d24
Harry <harryly140@gmail.com>
2022-06-28 10:08:07 -0500

    Completed assignment

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Linq;
 
 namespace Assignment
 {
     class Program
     {
+        private static string RemoveSimilarCharacters(string s)
+        {
+            string engAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            engAlphabet = string.Join("", engAlphabet.Where(ch => !s.Contains(ch)));
+
+            return engAlphabet;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter an input up to 256 characters: ");
+            string input = Console.ReadLine().ToUpper();
+
+            if (input.Length < 256)
+            {
+                Console.WriteLine($"Removed characters: {RemoveSimilarCharacters(input)}");
+                Console.WriteLine($"Amount of characters in list: {RemoveSimilarCharacters(input).Length}");
+            }
+            else
+            {
+                Console.Write("Input length has exceeded 256");
+            }
         }
     }
 }
